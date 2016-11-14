37ce93347d13ed0e7ac90015c9f88e39f8544d6a
Chad Bailey <chadbailey@gmail.com>
2016-11-14 05:37:00 +0000

    updated dotenv settings

--- weathergrabber/wsgi.py
+++ weathergrabber/wsgi.py
@@ -9,6 +9,11 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os
 
+# dotenv
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+# dotenv
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "weathergrabber.settings")
